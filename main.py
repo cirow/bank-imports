d6e43deff00384195088be23fbdce1cf81ce4b09
cirow <ciro.perez@glssolutions.us>
2026-04-11 21:39:42 -0300

    refactor: using uv as main tool

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ def cli():
 @cli.command('classify')
 @click.option('--filename', '-f', required=True, help='Filename')
 @click.option("--type", "-t", required=True, help="Type of statement: xp, mercadopago")
-def classify(filename, type):
-    filehandler.classify(filename, type)
+@click.option("--month", "-m",default=0, help="Filter by month (1 - 12)")
+@click.option("--year", "-y", default=0, help="Filter by year (default=current)")
+def classify(filename, type, month, year):
+    filehandler.classify(filename, type, month, year)
 
 if __name__ == '__main__':
     cli()
